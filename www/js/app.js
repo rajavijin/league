cbece873a313dc1cc8873d3a3974cd5a96d921cd
sahaya rex <rexdeveloper@gmail.com>
2016-03-21 17:13:47 +0530

    latest

--- www/js/app.js
+++ www/js/app.js
@@ -87,6 +87,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/chats');
 
 });
